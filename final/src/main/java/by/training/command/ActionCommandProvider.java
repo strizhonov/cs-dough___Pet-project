7577f91955e2b7428623dc554495dd664efe8dd4
strizhonov <vladlenstrizhonov@gmail.com>
2019-11-27 09:25:29 +0300

    some changes

--- final/src/main/java/by/training/command/ActionCommandProvider.java
+++
@@ -1,9 +0,0 @@
-package by.training.command;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface ActionCommandProvider {
-    ActionCommand get(HttpServletRequest request);
-
-    void register(ActionCommand... actionCommands);
-}
--- final/src/main/java/by/training/connection/ConnectionProvider.java
+++
@@ -1,9 +0,0 @@
-package by.training.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-@FunctionalInterface
-public interface ConnectionProvider {
-    Connection getConnection() throws SQLException;
-}
