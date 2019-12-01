e499cd7a95737de0f4cbb2f056303c9ef0cd43ef
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 06:43:03 +0000

    OrganizerAccessChecker.java edited

--- final/src/main/java/by/training/security/OrganizerAccessChecker.java
+++ final/src/main/java/by/training/security/OrganizerAccessChecker.java
@@ -6,14 +6,16 @@ import by.training.servlet.ServletRouter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class OrganizerAccessChecker implements SecurityService {
-    @Override
-    public boolean canExecute(ActionCommand actionCommand, HttpServletRequest request) {
-        return false;
-    }
+public class OrganizerAccessChecker implements AccessChecker {
+
 
     @Override
-    public ServletRouter route(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
-        return null;
-    }
+    public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
+        if (user != null && user.getOrganizerId != 0) {
+            return true;
+        } 
+        return false;
+    
 }
