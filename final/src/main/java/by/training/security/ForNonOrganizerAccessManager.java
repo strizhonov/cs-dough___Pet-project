2486579a4ff3601da89e4d9039b79d6678abcf6b
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 09:25:04 +0000

    ForNonOrganizerAccessManager.java edited

--- final/src/main/java/by/training/security/ForNonOrganizerAccessManager.java
+++ final/src/main/java/by/training/security/ForNonOrganizerAccessManager.java
@@ -8,12 +8,17 @@ import javax.servlet.http.HttpServletResponse;
 
 public class ForNonOrganizerAccessManager implements AccessManager {
 
-
     @Override
     public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
-        return user == null || user.getOrganizerId == 0;
-        
+        return user != null && user.getOrganizerId == 0;
+    }  
+    
+    @Override
+    public Optional<BaseRedirector> direct(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        return isAllowed() ? Optional.empty : Optional.of(new AbsoluteRedirector(""))
+    } 
+    
     
 }
