7b446cf649d2bb2e7f27d253092db62e32a3b777
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 06:24:01 +0000

    ForwardRouterState.java edited

--- final/src/main/java/by/training/servlet/ForwardRouterState.java
+++ final/src/main/java/by/training/servlet/ForwardRouterState.java
@@ -7,16 +7,18 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class ForwardRouterState implements RouterState {
+public class ForwardRouter implements HttpRouter {
 
+    private HttpServlet servlet;
     private String path;
 
-    public ForwardRouterState(String path) {
+    public ForwardRouter(HttpServlet servlet, String path) {
+        this.servlet = servlet;
         this.path = path;
     }
 
     @Override
-    public void proceed(HttpServletRequest request, HttpServletResponse response)
+    public void dispatch(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         RequestDispatcher dispatcher = servlet.getServletContext().getRequestDispatcher(path);
         dispatcher.forward(request, response);
