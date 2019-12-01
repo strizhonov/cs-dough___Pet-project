e34990e2ca73b9e1bc1cbfbb4121e19e167f1c0d
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 07:11:12 +0000

    SecurityType.java edited

--- final/src/main/java/by/training/security/SecurityType.java
+++ final/src/main/java/by/training/security/SecurityType.java
@@ -1,12 +1,18 @@
 package by.training.security;
 
 public enum SecurityType {
-    ALL,
-    ONLY_ANONYMOUS,
-    NON_ORGANIZER_USERS,
-    NON_PLAYER_USERS,
-    USERS,
-    ORGANIZER,
-    PLAYER,
-    ADMIN
+    ALL(ForAllAccessChecker.class),
+    ONLY_ANONYMOUS(ForAnonymousAccessChecker.class),
+    NON_ORGANIZER_USERS(ForNonOrganizerAccessChecker.class),
+    NON_PLAYER_USERS(ForNonPlayerAccessChecker.class),
+    USER(ForUserAccessChecker.class),
+    ORGANIZER(ForOrganizerAccessChecker.class),
+    PLAYER(ForPlayerAccessChecker.class),
+    ADMIN(ForAdminAccessChecker.class);
+    
+    Clazz<?> clazz;
+    
+    SecurityType(Class<?> clazz) {
+        this.clazz = clazz;
+    }
 }
