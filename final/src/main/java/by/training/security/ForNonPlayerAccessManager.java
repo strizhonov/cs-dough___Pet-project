d7e8c86b403d42cbf08ffdfeaf86d41db50747a3
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 09:43:03 +0000

    ForNonPlayerAccessManager.java edited

--- final/src/main/java/by/training/security/ForNonPlayerAccessManager.java
+++ final/src/main/java/by/training/security/ForNonPlayerAccessManager.java
@@ -1,19 +1,36 @@
-+package by.training.security;
-+
-+import by.training.command.ActionCommand;
-+import by.training.servlet.ServletRouter;
-+
-+import javax.servlet.http.HttpServletRequest;
-+import javax.servlet.http.HttpServletResponse;
-+
-+public class ForNonPlayerAccessManager implements AccessManager {
-+
-+
-+    @Override
-+    public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
-+        HttpSession session = request.getSession();
-+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
-+        return user == null || user.getPlayerId == 0;
-+        
-+    
-+}
+package by.training.security;
+
+import by.training.command.ActionCommand;
+import by.training.servlet.ServletRouter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class ForNonPlayerAccessSupervisor implements SecuritySupervisor {
+
+
+    @Override
+    public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
+        return user == null || user.getPlayerId == 0;
+	}
+	
+	@Override
+    public Optional<BaseRedirector> direct(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
+        if (isAccessAllowed()) {
+            return Optional.empty();
+        }
+        if (user == null) {
+           String message = 
+           req.setParameter(message)
+        } else if (user.getPlayerId != 0) {
+            String message = 
+        }
+        return Optional.of(new AbsoluteRedirector("Referer"));
+    }
+        
+    
+}
