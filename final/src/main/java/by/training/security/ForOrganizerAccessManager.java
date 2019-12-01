0c3a38994a4ebfc7c658314b966d0dd1ca1e5c0c
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 09:58:34 +0000

    ForOrganizerAccessManager.java edited

--- final/src/main/java/by/training/security/ForOrganizerAccessManager.java
+++ final/src/main/java/by/training/security/ForOrganizerAccessManager.java
@@ -6,15 +6,36 @@ import by.training.servlet.ServletRouter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class ForOrganizerAccessChecker implements AccessChecker {
+public class ForOrganizerOwnerAccessSupervisor implements SecuritySupervisor {
 
 
     @Override
     public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        String sOrganizerId = request.getParameter(AttributesContainer.ORGANIZER_TO_OPERATE_ID.toString());
         HttpSession session = request.getSession();
         UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
         OrganizerDto organizerToOperate = (OrganizerDto) session.getAttribute(AttributesContainer.ORGANIZER_TO_OPERATE.toString());
-        return user != null && user.getOrganizerId() == organizerToOperate.getId();
+        long organizerId = sOrganizerId != null ? Long.parse(sOrganizerId) : organizerToOperate != null ? organizerToOperate.getId : 0;              
+        return user != null && user.getOrganizerId() == organizerId();
+    }
+        
+    @Override
+    public Optional<BaseRedirector> direct(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+        String sOrganizerId = request.getParameter(AttributesContainer.ORGANIZER_TO_OPERATE_ID.toString());
+        HttpSession session = request.getSession();
+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
+        OrganizerDto organizerToOperate = (OrganizerDto) session.getAttribute(AttributesContainer.ORGANIZER_TO_OPERATE.toString());
+        long organizerId = sOrganizerId != null ? Long.parse(sOrganizerId) : organizerToOperate != null ? organizerToOperate.getId : 0;  
+        if (isAccessAllowed()) {
+            return Optional.empty();
         }
+        if (user == null) {
+           String message = 
+           req.setParameter(message)
+        } else if (user.getOrganizerId != organizerId) {
+            String message = 
+        }
+        return Optional.of(new AbsoluteRedirector("Referer"));
+    }
     
 }
