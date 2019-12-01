cd379867692398318545132ac3c519e9cb1eaec2
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 11:08:57 +0000

    ForAnyOrganizerAccessType.java edited

--- final/src/main/java/by/training/security/ForAnyOrganizerAccessType.java
+++ final/src/main/java/by/training/security/ForAnyOrganizerAccessType.java
@@ -6,16 +6,33 @@ import by.training.servlet.ServletRouter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class OrganizerAccessChecker implements AccessChecker {
+public class ForAnyOragnizerTypeSupervisor implements SecuritySupervisor {
 
 
     @Override
-    public boolean isAccessAllowed(ActionCommand command, HttpServletRequest request, HttpServletResponse response) {
+    public boolean isAccessAllowed(HttpServletRequest request, HttpServletResponse response) {
         HttpSession session = request.getSession();
         UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
         if (user != null && user.getOrganizerId != 0) {
             return true;
         } 
         return false;
+        
+        
+    @Override
+    public Optional<BaseRedirector>(HttpServletRequest request, HttpServletResponse response) {
+        HttpSession session = request.getSession();
+        UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
+        if (isAccessAllowed) {
+            return Optional.empty();
+        }
+        if (user == null) {
+        String alertMessage = 
+        
+        } else if (user.getOrganizerId == 0) {
+        alertMessage = ;
+        }
+        
+        return Optional.of(new Redirector("referer"));
     
 }
