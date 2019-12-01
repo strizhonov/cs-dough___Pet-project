f87d0b4b00fb24570dc669dcb79f972ed790a21a
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 06:13:49 +0000

    AbsoluteRedirectRouter.java edited

--- final/src/main/java/by/training/servlet/AbsoluteRedirectRouter.java
+++ final/src/main/java/by/training/servlet/AbsoluteRedirectRouter.java
@@ -5,7 +5,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-public class AbsoluteRedirectRouterState implements RouterState {
+public class AbsoluteRedirectRouter implements HttpRouter {
 
     private String path;
 
