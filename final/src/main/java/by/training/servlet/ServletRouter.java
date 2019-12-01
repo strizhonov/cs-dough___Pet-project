fb2d7a69e3d035059731d4ac127da0d951a5070a
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 06:03:04 +0000

    ServletRouter.java edited

--- final/src/main/java/by/training/servlet/ServletRouter.java
+++ final/src/main/java/by/training/servlet/ServletRouter.java
@@ -1,56 +1,9 @@
-package by.training.servlet;
+fapackage by.training.servlet;
 
 import by.training.constant.PathsContainer;
 
-public class ServletRouter //interface {
+public interface ServletRouter {
 
-    private String path;
-    private RouterState state;
-
-    public ServletRouter() {
-        this.path = PathsContainer.INDEX_PAGE_PATH;
-        this.state = new ForwardRouterState(path);
-    }
-
-    public ServletRouter(String path) {
-        this.path = path;
-        this.state = new ForwardRouterState(path);
-    }
-
-    public ServletRouter(String path, RouterType routerType) {
-        this.path = path;
-        if (routerType == RouterType.FORWARD) {
-            this.state = new ForwardRouterState(path);
-        } else if (routerType == RouterType.RELATIVE_REDIRECT) {
-            this.state = new RelativeRedirectRouterState(path);
-        } else if (routerType == RouterType.ABSOLUTE_REDIRECT) {
-            this.state = new AbsoluteRedirectRouterState(path);
-        }
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public void setPath(String path) {
-        this.path = path;
-    }
-
-    public RouterState getState() {
-        return state;
-    }
-
-    public void setState(RouterType routerType) {
-        if (routerType == RouterType.FORWARD) {
-            this.state = new ForwardRouterState(path);
-        } else if (routerType == RouterType.RELATIVE_REDIRECT) {
-            this.state = new RelativeRedirectRouterState(path);
-        } else if (routerType == RouterType.ABSOLUTE_REDIRECT) {
-            this.state = new AbsoluteRedirectRouterState(path);
-        }
-    }
-
-    public enum RouterType {
-        FORWARD, RELATIVE_REDIRECT, ABSOLUTE_REDIRECT
-    }
+    void dispatch(HttpRequest request, HttpResponse response);
+    
 }
