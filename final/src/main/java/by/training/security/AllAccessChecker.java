5257183ab174dce52288717a49063e168e78b27f
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 09:10:32 +0000

    AllAccessChecker.java edited online with Bitbucket

--- final/src/main/java/by/training/security/AllAccessChecker.java
+++ final/src/main/java/by/training/security/AllAccessChecker.java
@@ -4,9 +4,15 @@ import by.training.command.ActionCommand;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class AllAccessChecker implements AccessChecker {
+public class ForAllAccessSupervisor implements SecuritySupervisor {
+    
     @Override
     public boolean isAccessAllowed(ActionCommand actionCommand, HttpServletRequest request) {
         return true;
+    }
+    
+    @Override
+    public Optional<BaseRedirector> direct(ActionCommand actionCommand, HttpServletRequest request, HttpServletResponse response) {
+        return Optional.empty();
     }
 }
