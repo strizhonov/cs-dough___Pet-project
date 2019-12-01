772bad10d1139f5c635801705a60ac711755579f
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 10:04:04 +0000

    CommandAccessChecker.java created

--- final/src/main/java/by/training/security/CommandAccessChecker.java
+++ final/src/main/java/by/training/security/CommandAccessChecker.java
@@ -1,12 +1,7 @@
 package by.training.security;
 
-import by.training.command.ActionCommand;
-
-import javax.servlet.http.HttpServletRequest;
-
-@FunctionalInterface
 public interface CommandAccessChecker {
 
-    boolean isAccessAllowed(ActionCommand actionCommand, HttpServletRequest request);
+    boolean isAccessAllowed();
 
 }
