fc26291a4992c591014e858bdc8310cbf41fca32
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-01 09:17:03 +0000

    AnonymousAccessChecker.java edited

--- final/src/main/java/by/training/security/AnonymousAccessChecker.java
+++ final/src/main/java/by/training/security/AnonymousAccessChecker.java
@@ -4,12 +4,18 @@ import by.training.command.ActionCommand;
 
 import javax.servlet.http.HttpServletRequest;
 
-public class AnonymousAccessChecker implements AccessChecker {
-@Override
+public class ForAnonymousAccessSupervisor implements SecuritySupervisor {
+    
+    @Override
     public boolean isAccessAllowed(ActionCommand actionCommand, HttpServletRequest request) {
         HttpSession session = request.getSession();
         UserDto user = (User) session.getAttribute(AttributesContainer.USER.toString());
         return user == null;
+    }
+    
+    @Override
+    public Optional<BaseRedirector> direct(ActionCommand actionCommand, HttpServletRequest request) {
+        return isAccessAllowed : Optional.empty() : Optional.of(new RelativeRedirector(response.getHeader("Referer")));
     }
 
 }
