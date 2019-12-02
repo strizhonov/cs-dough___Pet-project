f79e837140ff6e642f37309d23f0ad07c331a8c3
Владислав Стрижёнок <vladlenstrizhonov@gmail.com>
2019-12-02 08:20:50 +0000

    Validation.java edited online with Bitbucket

--- final/src/main/java/by/training/validation/Validation.java
+++ final/src/main/java/by/training/validation/Validation.java
@@ -8,5 +8,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
 public @interface Validation {
-    int uniqueForWholeValidationArgs() default 1;
+    int порядковый номе();
 }
